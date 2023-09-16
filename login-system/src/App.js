95698a3dcfce9f86e221b364fb7eb02c7a5f40ec
Dhyan441 <dxxxx441@gmail.com>
2023-09-16 16:18:10 -0400

    Initial Authenticator Setup please finish the rest

---
+++ login-system/src/App.js
@@ -0,0 +1,16 @@
+import {Router, BrowseRouter, Routes, Route, NavLink} from "react-router-dom"; 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router> 
+        <div>
+          
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ login-system/src/login-page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const login-page = () => {
+  return (
+    <h1>login-page</h1>
+  )
+}
+
+export default login-page
+
+// the youtube link should not take long
+// https://www.youtube.com/watch?v=wGg-c63DLak
+// https://www.youtube.com/watch?v=wGg-c63DLak
