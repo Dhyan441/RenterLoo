c12b4d9a0fd81819a8333302df730c19be926dcf
Saaniya19 <saaniya.saraf@gmail.com>
2023-09-16 22:13:43 -0400

    added find and post property pages

---
+++ ui-files/src/components/presentation/postProperty.tsx
@@ -0,0 +1,16 @@
+import ThemeProvider from "../theme-provider";
+import Navbar from "../defaultNavbar"
+ 
+import { Typography, Input, Button } from "@material-tailwind/react";
+
+export function PostProperty() {
+  return (
+    <ThemeProvider>
+      <Navbar />
+      
+    </ThemeProvider>
+  );
+}
+
+export default PostProperty;
+
